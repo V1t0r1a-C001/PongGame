c04543468f7477cd80e053c513388817f8b1291a
Vitória <vivi.sousa964@gmail.com>
2021-12-19 13:19:53 -0300

    inicio

---
+++ pong.js
@@ -0,0 +1,13 @@
+let xBolinha=100;
+let yBolinha=200;
+let diametro=22;
+
+
+function setup (){
+    createCanvas(600,400);
+}
+
+function draw(){
+    background(0);
+    circle(xBolinha,yBolinha,diametro)
+}
